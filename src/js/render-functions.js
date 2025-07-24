0d1f681aa86e63f720d3e5caf954b7f876bf12b4
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-07-24 12:13:38 +0200

    Update render-functions.js

---
+++ src/js/render-functions.js
@@ -0,0 +1,31 @@
+export function renderMarcup(data) {
+    return data
+      .map(
+        el =>
+          `<div class="gallery-item">
+                    <a class="gallery-link" href="${el.largeImageURL}">
+                        <img class="gallery-image" src="${el.webformatURL}" alt="${el.tags}" />
+                    </a>
+                    <ul class="gallery-item-info">
+                        <li class="gallery-item-info-par">
+                            <span class="gallery-item-info-span">Likes: <span>${el.likes}</span>
+                            </span>    
+                        </li>
+                        <li class="gallery-item-info-par">
+                            <span class="gallery-item-info-span">Views: <span>${el.views}</span>
+                            </span>    
+                        </li>
+                        <li class="gallery-item-info-par">
+                            <span class="gallery-item-info-span">Comments: <span>${el.comments}</span>
+                            </span>    
+                        </li>
+                        <li class="gallery-item-info-par">
+                            <span class="gallery-item-info-span">Downloads: <span>${el.downloads}</span>
+                            </span>    
+                        </li>
+                    </ul>
+                </div>`
+      )
+      .join('');
+  }
+  
